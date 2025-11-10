6765166c8287dbc8f308ff87722c46049929600b
TheMaz <patriczum@gmail.com2>
2025-11-10 09:51:07 +0200

    331323

--- app/Http/Controllers/SudoWebController.php
+++ app/Http/Controllers/SudoWebController.php
@@ -14,7 +14,7 @@ class SudoWebController extends Controller
 
     function provider()
     {
-        // defaultdata();
+        defaultdata();
         $item = request('item');
         if ($item) {
             $entity = Entity::where('shortname', $item)->first();

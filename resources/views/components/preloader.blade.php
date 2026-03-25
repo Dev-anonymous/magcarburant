393236ad12c122fa4c5167ede7804cceb2f582da
Patrick <pat@email.com>
2026-03-25 14:44:14 +0200

    43243423

--- resources/views/components/preloader.blade.php
+++ resources/views/components/preloader.blade.php
@@ -12,7 +12,7 @@
         z-index: 999999;
     }
 
-    .spinner {
+    /* .spinner {
         width: 60px;
         height: 60px;
         border: 6px solid #e0e0e0;
@@ -26,14 +26,107 @@
         100% {
             transform: rotate(360deg);
         }
+    } */
+
+
+
+
+    /* ///  */
+    .loader {
+        transform: rotateZ(45deg);
+        perspective: 1000px;
+        border-radius: 50%;
+        width: 100px;
+        height: 100px;
+        color: #fff;
+    }
+
+    .loader:before,
+    .loader:after {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: inherit;
+        height: inherit;
+        border-radius: 50%;
+        transform: rotateX(70deg);
+        animation: 1s spin linear infinite;
+    }
+
+    .loader:after {
+        color: #FF3D00;
+        transform: rotateY(70deg);
+        animation-delay: .4s;
+    }
+
+    @keyframes rotate {
+        0% {
+            transform: translate(-50%, -50%) rotateZ(0deg);
+        }
+
+        100% {
+            transform: translate(-50%, -50%) rotateZ(360deg);
+        }
+    }
+
+    @keyframes rotateccw {
+        0% {
+            transform: translate(-50%, -50%) rotate(0deg);
+        }
+
+        100% {
+            transform: translate(-50%, -50%) rotate(-360deg);
+        }
+    }
+
+    @keyframes spin {
+
+        0%,
+        100% {
+            box-shadow: .2em 0px 0 0px currentcolor;
+        }
+
+        12% {
+            box-shadow: .2em .2em 0 0 currentcolor;
+        }
+
+        25% {
+            box-shadow: 0 .2em 0 0px currentcolor;
+        }
+
+        37% {
+            box-shadow: -.2em .2em 0 0 currentcolor;
+        }
+
+        50% {
+            box-shadow: -.2em 0 0 0 currentcolor;
+        }
+
+        62% {
+            box-shadow: -.2em -.2em 0 0 currentcolor;
+        }
+
+        75% {
+            box-shadow: 0px -.2em 0 0 currentcolor;
+        }
+
+        87% {
+            box-shadow: .2em -.2em 0 0 currentcolor;
+        }
     }
 </style>
 
 <div>
     <div id="preloader">
-        <div class="spinner"></div>
+        {{-- <div class="spinner"></div> --}}
+        <span class="loader"></span>
     </div>
 </div>
+
+
+
 
 <script>
     window.addEventListener("load", function() {

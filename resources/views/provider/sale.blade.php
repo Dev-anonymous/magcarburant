35b34caf5826f16f3848220e3cdbb881c22ce780
TheMaz <patriczum@gmail.com2>
2026-04-01 21:11:46 +0200

    4234234

--- resources/views/provider/sale.blade.php
+++ resources/views/provider/sale.blade.php
@@ -144,8 +144,13 @@
     </div>
 @endsection
 @section('modals')
+    <style>
+        .modal-body {
+            overflow-y: auto !important;
+        }
+    </style>
     <div class="modal fade" id="mdladd" role="dialog">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="defaultModalLabel">Nouvelle vente</h5>
